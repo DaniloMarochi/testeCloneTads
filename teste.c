9b43cd9179db77361dbcffa57ea289594c1bb1b8
Helenamaia <helenadiasmaia@gmail.com>
2020-02-14 10:06:42 -0300

    impar par

--- teste.c
+++ teste.c
@@ -2,7 +2,17 @@
 #include <stdio.h>
 
 int main(){
+    int num;
+    
+    printf("\n\tDigite um número inteiro: ");
+    scanf("%i", &num);
 
-
+    if(num%2==0){
+        printf("\n\tNúmero par");
+    }
+    else{
+        printf("\n\tNúmeor impar");
+    }
+printf("\n");
     return 0;
 }
